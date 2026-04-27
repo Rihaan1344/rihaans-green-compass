4e4923fb5a5f6a3e1d2928ffacfcffa0fbf8e678
Rihaan1344 <syedmrihaan@gmail.com>
2026-04-27 11:05:35 +0530

    Added streamlit functionality to make tree project a website

--- tree_project_maker.py
+++ tree_project_maker.py
@@ -2,101 +2,142 @@ import requests as req
 from dotenv import load_dotenv
 import os
 import qrcode
+import streamlit as st
 
 load_dotenv()
 
 PLANT_API_KEY = os.getenv("PLANT_API_KEY")
 TEMPLATE_API_KEY = os.getenv("TEMPLATE_API_KEY")
 
-if not PLANT_API_KEY: raise ValueError("key not found bro. how lock open if not key?")
+if not PLANT_API_KEY or not TEMPLATE_API_KEY: raise ValueError("key not found bro. how lock open if not key?")
 
-bark_filepath = input("Please enter file path for the image of the bark of the tree: ")
-leaf_filepath = input("Please enter file path for the image of the leaf of the tree: ")
+st.title("Rihaan's Green Compass")
+st.markdown("Upload an image of the bark and the leaf, and you'll get a ready-made QR code which leads you to a beautiful green poster of the tree's scientific information!")
+st.divider()
+st.header("Upload images")
 
-url = f"https://my-api.plantnet.org/v2/identify/all?api-key={PLANT_API_KEY}"
+bark = st.file_uploader(
+   "Upload an image of the bark of the tree",
+   type = ["jpg", "png", "jpeg"]
+)   
 
-with open(bark_filepath, "rb") as bark, open(leaf_filepath, "rb") as leaf:
-    files = [
-        ("images", bark),
-        ("images", leaf)
-    ]
-    data = {
-    "organs": ["bark", "leaf"]
-    }
+leaf = st.file_uploader(
+   "Upload an image of the leaf of the tree",
+   type = ["jpg", "png", "jpeg"]
+)
 
-    response = req.post(url, files=files, data=data)
-    result = response.json()
+st.divider()
+if bark and leaf:
+   url = f"https://my-api.plantnet.org/v2/identify/all?api-key={PLANT_API_KEY}"
 
-   
-if "results" not in result or not result["results"]:
-    print("Could not identify, please try a clearer image")
+   files = [
+         ("images", bark.read()),
+         ("images", leaf.read())
+      ]
+   data = {
+      "organs": ["bark", "leaf"]
+      }
 
-else:
-    best = result["results"][0]
+   response = req.post(url, files=files, data=data)
+   result = response.json()
 
-species = best["species"]["scientificName"]
-genus = best["species"]["genus"]["scientificName"]
-family = best["species"]["family"]["scientificName"]
+   st.header("Plant classification")
 
-common_names = best["species"].get("commonNames", [])
-common_name = common_names[0] if common_names else "N/A"
+      
+   if "results" not in result or not result["results"]:
+      msg = "Could not identify, please try a clearer image"
+      print(msg)
+      st.markdown(msg)
+
+   else:
+      best = result["results"][0]
 
 
-template_id = 'bc04434b-ef5a-407e-ab5a-7d681d3d03b4'
+      species = best["species"]["scientificName"]
+      genus = best["species"]["genus"]["scientificName"]
+      family = best["species"]["family"]["scientificName"]
 
-url = 'https://api.templated.io/v1/render'
-headers = {
-  'Content-Type': 'application/json',
-  'Authorization': f'Bearer {TEMPLATE_API_KEY}'
-}
+      common_names = best["species"].get("commonNames", [])
+      common_name = common_names[0] if common_names else "N/A"
 
-data = {
-  'template': template_id,
-  'layers': {
-    'species-placeholder' : {
-       'text' : species
-    },
+      st.markdown(f"Species: {species}")
+      st.markdown(f"Genus: {genus}")
+      st.markdown(f"Family: {family}")
+      st.markdown(f"Common Name: {common_name}")
 
-    'genus-placeholder' : {
-       'text' : genus
-    },
+      print("-" * 20)
 
-    'family-placeholder' : {
-       'text' : family
-    },
+      print("Species:", species)
+      print("Genus:", genus)
+      print("Family:", family)
+      print("Common name:", common_name)
 
-    'common-names-placeholder' : {
-       'text' : common_name
-    },
+      st.divider()
+      st.header("Poster QR code")
 
-    'common-name' : {
-       'text' : common_name
-    },
-    
-    'scientific-name' : {
-       'text' : species
-    }
-    
-  }
-}
+      template_id = "bc04434b-ef5a-407e-ab5a-7d681d3d03b4"
 
-response = req.post(url, json=data, headers=headers)
-result = response.json()
+      url = 'https://api.templated.io/v1/render'
+      headers = {
+      'Content-Type': 'application/json',
+      'Authorization': f'Bearer {TEMPLATE_API_KEY}'
+      }
 
-if response.status_code == 200:
-  print('Render request accepted.')
-else:
-  print('Render request failed. Response code:', response.status_code)
-  print(response.text)
+      data = {
+      'template': template_id,
+      'layers': {
+         'species-placeholder' : {
+            'text' : species
+         },
 
-print("-" * 20)
+         'genus-placeholder' : {
+            'text' : genus
+         },
 
-print("Species:", species)
-print("Genus:", genus)
-print("Family:", family)
-print("Common name:", common_name)
+         'family-placeholder' : {
+            'text' : family
+         },
 
-data = result["url"]
-qr = qrcode.make(data)
-qr.save("poster_qr.png")
+         'common-names-placeholder' : {
+            'text' : common_name
+         },
 
+         'common-name' : {
+            'text' : common_name
+         },
+         
+         'scientific-name' : {
+            'text' : species
+         }
+         
+      }
+      }
+
+      response = req.post(url, json=data, headers=headers)
+
+
+      if response.status_code == 200:
+         msg = 'Render request accepted.'
+         print(msg)
+         st.markdown(msg)
+
+         result = response.json()
+
+         data = result["url"]
+         qr = qrcode.make(data)
+         path = os.path.join(os.getcwd(), "static", "poster_qr.png")
+         qr.save(path)
+
+         st.image(path)
+
+         st.markdown("Scan the QR code to view your poster")
+         st.caption(f"...or access it directly via ")
+         st.link_button("this link.", data)
+
+      else:
+         msg = f'Render request failed. Response code: {response.status_code}'
+         print(msg)
+         print(response.text)
+
+         st.markdown(msg)
+         st.markdown(response.text)
